8af8478f00cdc9ee4d678d7433a8e98c855a6fc9
Dmitry Dolzhenko <d.dolzhenko@gmail.com>
2016-03-31 21:45:30 +0300

    fixed checksum

--- checksum.py
+++ checksum.py
@@ -1,5 +1,5 @@
 from checksumdir import dirhash
 
 if __name__ == '__main__':
-    line = "hash(./test) = <{}>".format(dirhash('test'))
+    line = "hash(./test) = <{}>".format(dirhash('test', 'sha1'))
     print (line)
